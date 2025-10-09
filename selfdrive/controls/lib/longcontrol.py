8e12e08a8b95c9db9e5a8aab3140f0e90a1b959a
roenthomas <43324106+roenthomas@users.noreply.github.com>
2025-10-09 16:28:11 -0400

    StarPilot longcontrol update_mpc changes
    Partially integrated firestar5683 commit https://github.com/firestar5683/StarPilot/commit/1cdf6ccd04cf1c2753ff7ba60f4a90d6c7c5839d#diff-3da960eae4d7eb8892b4f762397a17b7ef82dcb24c7d6391a76b2ea7cb04dd7b
    
    Incorporated longcontrol.py changes to help long control
    
    Updated selfdrive/controls/lib/longcontrol.py
    
    Co-Authored-By: firestar5683 <168790843+firestar5683@users.noreply.github.com>

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -4,6 +4,8 @@ from openpilot.common.realtime import DT_CTRL
 from openpilot.selfdrive.controls.lib.drive_helpers import CONTROL_N, apply_deadzone
 from openpilot.selfdrive.controls.lib.pid import PIDController
 from openpilot.selfdrive.modeld.constants import ModelConstants
+from openpilot.common.filter_simple import FirstOrderFilter
+from openpilot.selfdrive.car.gm.values import CarControllerParams 
 
 CONTROL_N_T_IDX = ModelConstants.T_IDXS[:CONTROL_N]
 
@@ -90,13 +92,42 @@ class LongControl:
   def __init__(self, CP):
     self.CP = CP
     self.long_control_state = LongCtrlState.off
+    self.experimental_mode = False
     pos_p_limit = 0.0 # if params("NoPositivePResponse") else None # put parameter-based control here
     self.pid = PIDController((CP.longitudinalTuning.kpBP, CP.longitudinalTuning.kpV),
                              (CP.longitudinalTuning.kiBP, CP.longitudinalTuning.kiV),
                              k_f=CP.longitudinalTuning.kfDEPRECATED, rate=1 / DT_CTRL,
                              pos_p_limit=pos_p_limit)
     self.v_pid = 0.0
+    self._mode_setup()
     self.last_output_accel = 0.0
+
+  def update_mpc_mode(self, experimental_mode):
+    new_mode = 'blended' if experimental_mode else 'acc'
+
+    if self.transitioning and self.prev_mode == 'blended' and self.current_mode == 'acc':
+      self.mode_transition_timer = 0.0
+
+    if new_mode != self.current_mode:
+      self.prev_mode = self.current_mode
+      self.transitioning = True
+      self.mode_transition_timer = 0.0
+      self.mode_transition_filter.x = self.last_output_accel
+
+      self.current_mode = new_mode
+
+    if self.transitioning:
+      self.mode_transition_timer += DT_CTRL
+      if self.mode_transition_timer >= self.mode_transition_duration:
+        self.transitioning = False
+
+  def _mode_setup(self):
+    self.prev_mode = 'acc'
+    self.current_mode = 'acc'
+    self.mode_transition_filter = FirstOrderFilter(0.0, 0.5, DT_CTRL)
+    self.mode_transition_timer = 0.0
+    self.mode_transition_duration = 1.0
+    self.transitioning = False
 
   def reset(self):
     self.pid.reset()
