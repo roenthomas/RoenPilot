a841cfe92350bdad9cd4f06bd8f6ba7ab27f9d4b
roenthomas <43324106+roenthomas@users.noreply.github.com>
2025-10-24 01:16:01 -0400

    Refactor: replace if-else block with conditional expression in limit_accel_in_turns()
    Why use multiple lines when one line will do clearly?

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -45,11 +45,7 @@ def limit_accel_in_turns(v_ego, angle_steers, a_target, CP):
   # The lookup table for turns should also be updated if we do this
   a_total_max = interp(v_ego, _A_TOTAL_MAX_BP, _A_TOTAL_MAX_V)
   a_y = v_ego ** 2 * angle_steers * CV.DEG_TO_RAD / (CP.steerRatio * CP.wheelbase)
-
-  if abs(a_y) > MINIMUM_LATERAL_ACCELERATION:
-    a_x_allowed = sqrt(max(a_total_max ** 2 - a_y ** 2, 0.))
-  else:
-    a_x_allowed = a_target[1]
+  a_x_allowed = sqrt(max(a_total_max ** 2 - a_y ** 2, 0.)) if abs(a_y) > MINIMUM_LATERAL_ACCELERATION else a_target[1]
 
   return [a_target[0], min(a_target[1], a_x_allowed)]
 
