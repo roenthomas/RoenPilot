37c6a62224212374b9738cb3bb511147bf431a59
roenthomas <43324106+roenthomas@users.noreply.github.com>
2025-10-28 00:39:57 -0400

    Reduce min_steer_speed to 0 for torque modded Honda Clarity
    Expanded speed support

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -202,7 +202,7 @@ class CAR(Platforms):
     flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES,
   )
   HONDA_CLARITY = HondaNidecPlatformConfig(
-    [HondaCarDocs("Honda Clarity 2018-22", "All", min_steer_speed=3. * CV.MPH_TO_MS)],
+    [HondaCarDocs("Honda Clarity 2018-22", "All", min_steer_speed=0. * CV.MPH_TO_MS)],
     CarSpecs(mass=4052. * CV.LB_TO_KG, wheelbase=2.75, centerToFrontRatio=0.41, steerRatio=16.50, tireStiffnessFactor=1.),
     dbc_dict('honda_clarity_hybrid_2018_can_generated', 'acura_ilx_2016_nidec'),
     rp_flags=HondaRoenPilotFlags.NIDEC_PEDAL_TUNE,
