47426e794155a8dcfe0907d0c72342efe3388b7c
roenthomas <43324106+roenthomas@users.noreply.github.com>
2025-11-18 17:07:50 -0500

    Restore original Increase Nidec Honda Braking Strength
    Integrated mike86437 commit https://github.com/mike86437/openpilot/commit/c51227f1ba8d78a1d8952b543636897bfbc3d0dd
    
    Co-Authored-By: mike8643 <98910897+mike8643@users.noreply.github.com>

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -13,6 +13,8 @@ from openpilot.selfdrive.controls.lib.drive_helpers import rate_limit
 VisualAlert = car.CarControl.HUDControl.VisualAlert
 LongCtrlState = car.CarControl.Actuators.LongControlState
 
+_BrakeModifier = 0.0
+
 
 def compute_gb_honda_bosch(accel, speed):
   # TODO returns 0s, is unused
@@ -20,16 +22,23 @@ def compute_gb_honda_bosch(accel, speed):
 
 
 def compute_gb_honda_nidec(accel, speed, frogpilot_toggles):
+  global _BrakeModifier
+  if accel < -3.9:
+    _BrakeModifier += 0.01
+  else:
+    _BrakeModifier = 0.0
   creep_brake = 0.0
   creep_speed = 2.3
   creep_brake_value = 0.15
   if speed < creep_speed:
     creep_brake = (creep_speed - speed) / creep_speed * creep_brake_value
-  gb = float(accel) / 4.8 - creep_brake
 
   if frogpilot_toggles.honda_nidec_max_brake:
-    return clip(gb, 0.0, 1.0), clip(float(accel) / -4.0, 0.0, 1.0)
+    gb = float(accel) / interp(float(accel), [4.0, 3.5], [4.0, 4.8]) - creep_brake
+    just_brake = float(accel) / (-4.8 + _BrakeModifier) + creep_brake
+    return clip(gb, 0.0, 1.0), clip(just_brake, 0.0, 1.0)
   else:
+    gb = float(accel) / 4.8 - creep_brake
     return clip(gb, 0.0, 1.0), clip(-gb, 0.0, 1.0)
 
 
